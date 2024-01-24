914e6ede107df2d8ce9e08559d74729883e3e268
rabeel.qaiser <rabeel.qaiser@sparkai.ca>
2024-01-24 16:33:06 +0500

    Cleaning Code: Remove unnecessary code 2

--- app/videoPage/[...id]/page.jsx
+++ app/videoPage/[...id]/page.jsx
@@ -21,9 +21,6 @@ const VideoPage = () => {
     } = useParams()
     const { width: windowWidth } = useWindowDimensions()
     const { data: videoData, isLoading: isLoadingVideoData } = useVideoById(videoId, VideoType[selectedTitle])
-    // console.log('videoData', videoData)
-    // console.log('prop category', videoId)
-    // console.log('prop region', VideoType[selectedTitle])
 
     const onReady = (event) => {
         // access to player in all event handlers via event.target
@@ -79,46 +76,62 @@ const VideoPage = () => {
                         className={`YoutubeMainDiv w-full mobile:object-cover xl:rounded-tl-lg xl:rounded-tr-lg mt-[6rem] mobile:mt-[4.5rem] md:mt-[4.5rem] lg:mt-[5.2rem] 2xl:mt-[6.3rem] overflow-hidden`}
                     >
                         {VideoType[selectedTitle]?.length > 0 &&
-                            !!VideoType[selectedTitle] &&
-                            !!videoId &&
-                            videoId.length > 0 && (
-                                <YouTube
-                                    className="YOUTUBE_PLAYER"
-                                    videoId={videoId}
-                                    opts={{
-                                        // height: calculatedHeight(windowWidth),
-                                        height:
-                                            windowWidth < 425
-                                                ? '200px'
-                                                : windowWidth < 600
-                                                ? '400px'
-                                                : windowWidth < 900
-                                                ? '500px'
-                                                : windowWidth < 1024
-                                                ? '500px'
-                                                : windowWidth < 1440
-                                                ? '600px'
-                                                : windowWidth < 1600
-                                                ? '750px'
-                                                : windowWidth < 2560
-                                                ? '1200px'
-                                                : '1000px',
-                                        width: '100%',
-                                        playerVars: {
-                                            autoplay: 0,
-                                        },
-                                    }}
-                                    onReady={onReady}
-                                    onPlay={onPlay}
-                                    onPause={onPause}
-                                    onEnd={onEnd}
-                                    onError={onError}
-                                    onStateChange={onStateChange}
-                                    style={{
-                                        margin: '0rem 0rem',
-                                    }}
-                                />
-                            )}
+                        !!VideoType[selectedTitle] &&
+                        !!videoId &&
+                        videoId.length > 0 &&
+                        !isLoadingVideoData ? (
+                            <YouTube
+                                className="YOUTUBE_PLAYER"
+                                videoId={videoId}
+                                opts={{
+                                    // height: calculatedHeight(windowWidth),
+                                    height:
+                                        windowWidth < 425
+                                            ? '200px'
+                                            : windowWidth < 600
+                                            ? '400px'
+                                            : windowWidth < 900
+                                            ? '500px'
+                                            : windowWidth < 1024
+                                            ? '500px'
+                                            : windowWidth < 1440
+                                            ? '600px'
+                                            : windowWidth < 1600
+                                            ? '750px'
+                                            : windowWidth < 2560
+                                            ? '1200px'
+                                            : '1000px',
+                                    width: '100%',
+                                    playerVars: {
+                                        autoplay: 0,
+                                    },
+                                }}
+                                onReady={onReady}
+                                onPlay={onPlay}
+                                onPause={onPause}
+                                onEnd={onEnd}
+                                onError={onError}
+                                onStateChange={onStateChange}
+                                style={{
+                                    margin: '0rem 0rem',
+                                }}
+                            />
+                        ) : (
+                            <div
+                                style={{ width: '100%' }}
+                                className="flex items-center justify-center w-full h-[420px] bg-gray-300 rounded sm:w-96 dark:bg-gray-700"
+                            >
+                                <svg
+                                    class="w-10 h-10 text-gray-200 dark:text-gray-600"
+                                    aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="currentColor"
+                                    viewBox="0 0 20 18"
+                                >
+                                    <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
+                                </svg>
+                            </div>
+                        )}
                     </div>
                     {!isLoadingVideoData ? (
                         <>
