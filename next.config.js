d4d0c65d924dd8b85e55d47b5d72516f5348bcdc
M-Ali-Haider <mah01134212100@gmail.com>
2024-08-20 20:44:37 +0500

    ...

--- next.config.js
+++ next.config.js
@@ -1,17 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: '',
-    distDir: 'build',
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'i.ytimg.com',
-                // port: '',
-                // pathname: '/account123/**',
-            },
-        ],
-    },
+    // basePath: '',
+    // distDir: 'build',
+    // images: {
+    //     remotePatterns: [
+    //         {
+    //             protocol: 'https',
+    //             hostname: 'i.ytimg.com',
+    //             // port: '',
+    //             // pathname: '/account123/**',
+    //         },
+    //     ],
+    // },
 }
 
 module.exports = nextConfig
