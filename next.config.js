381635f9e7a51a061132e003a806e5124a5f1dc1
M-Ali-Haider <mah01134212100@gmail.com>
2024-08-20 20:51:27 +0500

    meri images

--- next.config.js
+++ next.config.js
@@ -2,16 +2,16 @@
 const nextConfig = {
     // basePath: '',
     // distDir: 'build',
-    // images: {
-    //     remotePatterns: [
-    //         {
-    //             protocol: 'https',
-    //             hostname: 'i.ytimg.com',
-    //             // port: '',
-    //             // pathname: '/account123/**',
-    //         },
-    //     ],
-    // },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'i.ytimg.com',
+                // port: '',
+                // pathname: '/account123/**',
+            },
+        ],
+    },
 }
 
 module.exports = nextConfig
