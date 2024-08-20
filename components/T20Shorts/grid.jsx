43c5189018e2f864576995e8358f234d5d08b241
M-Ali-Haider <mah01134212100@gmail.com>
2024-08-20 17:46:39 +0500

    improvement: fixed shorts grid view

--- components/T20Shorts/grid.jsx
+++ components/T20Shorts/grid.jsx
@@ -3,7 +3,7 @@ import T20ShortCard from './card'
 const T20ShortsGrid = ({ data }) => {
     return (
         <>
-            <div className="mt-[25px] grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 xl:grid-cols-8 gap-4">
+            <div className="mt-[25px] grid grid-cols-2 xs:grid-cols-4 md:grid-cols-5 xl:grid-cols-8 gap-4">
                 {data['0'].map((item, index) => (
                     <T20ShortCard key={index} data={item} />
                 ))}
