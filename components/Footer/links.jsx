c9c2924ab3247fec23472651af9fb7388af5f9c1
M-Ali-Haider <mah01134212100@gmail.com>
2024-08-27 12:53:32 +0500

    added select none to footer links

--- components/Footer/links.jsx
+++ components/Footer/links.jsx
@@ -4,7 +4,7 @@ const FooterLinks = () => {
     return (
         <div className="flex flex-col sm:flex-row flex-wrap sm:items-center justify-between gap-4">
             {options.map((item, index) => (
-                <div onClick={() => handleFilterOption(item.id)} className="cursor-pointer" key={index}>
+                <div onClick={() => handleFilterOption(item.id)} className="cursor-pointer select-none" key={index}>
                     {item.title}
                 </div>
             ))}
