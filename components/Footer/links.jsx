73f6bb80f36fa669bcf6fa063cd028aee1a905d8
M-Ali-Haider <mah01134212100@gmail.com>
2024-08-27 13:26:36 +0500

    fixed footer

--- components/Footer/links.jsx
+++ components/Footer/links.jsx
@@ -2,7 +2,10 @@
 import { handleFilterOption, options } from '@/utils/filterbar'
 const FooterLinks = () => {
     return (
-        <div className="flex flex-col sm:flex-row flex-wrap sm:items-center justify-between gap-4">
+        <div
+            className="grid grid-cols-1 xs:grid-cols-2
+            md:flex md:items-center md:justify-between gap-4"
+        >
             {options.map((item, index) => (
                 <div onClick={() => handleFilterOption(item.id)} className="cursor-pointer select-none" key={index}>
                     {item.title}
