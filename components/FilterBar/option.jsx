d275109b9782b9d3c9df56b31fc1159f101e63d9
M-Ali-Haider <mah01134212100@gmail.com>
2024-08-29 11:05:40 +0500

    .........

--- components/FilterBar/option.jsx
+++ components/FilterBar/option.jsx
@@ -4,7 +4,9 @@ const FilterOption = ({ title }) => {
     const activeLink = useSelector((state) => state.activeLink.activeLink)
     return (
         <div
-            className={`${styles.toSectionButton} ${activeLink === title ? `before:opacity-100` : `before:opacity-0`}
+            className={`${styles.toSectionButton} ${
+                activeLink === title ? `before:opacity-100 text-white` : `before:opacity-0`
+            }
             text-xs lg:text-base cursor-pointer select-none
             py-3 px-5 lg:px-6`}
         >
